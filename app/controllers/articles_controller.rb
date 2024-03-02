deea84d073f46452fa2db66b45f6a35571d8ed45
Yourloff <alex.yourloff@gmail.com>
2024-03-02 22:42:25 +0700

    Добавлен показ(show) Article. Исправлена опечатка

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   def index
-    @artciles = Article.all
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
